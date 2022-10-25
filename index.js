0ab51ab9b8fcdbbedcdb101124ae8ca8197aace9
emilytm <113457923+emilytm@users.noreply.github.com>
2022-10-25 10:39:19 -0400

    Added round count info

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let totalEl = document.getElementById("total-el")
 let scoreEl = document.getElementById("score-el")
+let roundsEl = document.getElementById("rounds-el")
 let currentInc = 1
 let roundScore = 0
 let runningTotal = 0
+let roundNum = 0
 
 function increment() {
     roundScore += currentInc
@@ -15,6 +17,8 @@ function save() {
     totalEl.textContent = runningTotal
     scoreEl.textContent = 0
     roundScore = 0
+    roundNum += 1
+    roundsEl.textContent = roundNum
 }
 
 function decrement() {
@@ -26,11 +30,14 @@ function decrement() {
 function updateIncrement() {
     currentInc = parseInt(document.getElementById("inc").value)
 }
+
 function reset() {
     currentInc = 1
     roundScore = 0
     runningTotal = 0
+    roundNum = 0
     document.getElementById("inc").value = currentInc
     totalEl.textContent = runningTotal
     scoreEl.textContent = roundScore
+    roundsEl.textContent = roundNum
 }
