be3a4b23597e70b43368ce189d7ca29db5ad1860
emilytm <113457923+emilytm@users.noreply.github.com>
2022-10-25 10:12:51 -0400

    Uploading Scrimba Module 3 Project Files

---
+++ index.js
@@ -0,0 +1,28 @@
+let totalEl = document.getElementById("total-el")
+let scoreEl = document.getElementById("score-el")
+let currentInc = 1
+let roundScore = 0
+let runningTotal = 0
+
+function increment() {
+    roundScore += currentInc
+    scoreEl.textContent = roundScore
+}
+
+function save() {
+    runningTotal += roundScore
+    console.log(runningTotal)
+    totalEl.textContent = runningTotal
+    scoreEl.textContent = 0
+    roundScore = 0
+}
+
+function decrement() {
+    roundScore = roundScore - currentInc
+    console.log(currentInc)
+    scoreEl.textContent = roundScore
+}
+
+function updateIncrement() {
+    currentInc = parseInt(document.getElementById("inc").value)
+}
